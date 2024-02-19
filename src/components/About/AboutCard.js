4d9d6cd43c764e092fe2f5e15df5840902be5875
khoar <khoaruoi69@gmail.com>
2024-02-19 21:37:43 +0700

    ok

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,12 +9,8 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Khoa Nguyen </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            from <span className="purple"> Kon Tum, Viet Nam.</span>
             <br /> I am software engineer at company Bstar Solutions.
-            <br />
-            Additionally, I am currently employed as a software developer at
-            Juspay.
-            <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
