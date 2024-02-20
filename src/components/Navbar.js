95c559a50bd14e517a4e85e065ca79cf3fb8c707
Khoa Nguyen Dang Anh <khoa.nguyen.d@bstarsolutions.com>
2024-02-20 15:21:09 +0700

    changes code nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href="https://khoaruoi69.github.io/khoaNguyenDev/"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href=""
                 target="_blank"
                 className="fork-btn-inner"
               >
